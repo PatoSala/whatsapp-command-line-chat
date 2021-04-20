b4d4576c25937feac008032ef513e3052e91a91b
PatoSala <patosala998@gmail.com>
2021-04-20 14:18:45 -0300

    added commands

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Send and recieve WhatsApp messages through the command line.
 
 ## Step 1
 Before cloneing the repo use:  
-``` cd wpp-command-line-chat ```  
-``` npm install ```  
-``` node index.js ```
-&nbsp;
+&nbsp;``` cd wpp-command-line-chat ```  
+&nbsp;``` npm install ```  
+&nbsp;``` node index.js ```
+
 ## Step 2
 Once iniciated, wait until a QR code appears on your command line. Scan it with your WhatsApp camera.
 Wait until the message ``` Client is ready! ``` shows up. Now You'r ready to send and recieve WhatsApp Messages through your terminal.
@@ -17,3 +17,10 @@ You can send messages by typing first the number you want to text to and then th
 For example:  
 - (country code)+(that weird number between the country and area code)+(area code)+(phone number)  
 - 54911********
+
+# Commands  
+You can use certain commands by typing them in the ```phone``` prompt:  
+- /help --> Displays a list of available commands.  
+- /contacts --> Displays a list of your saved contacts.  
+- /addContact --> Adds a contact.  
+- /msg --> Send message to a number in your contacts list by typing their name.  
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { Client } = require('whatsapp-web.js');
 
 const client = new Client();
 
+let contacts = [];
+
 client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
 });
@@ -15,15 +17,68 @@ client.on('ready', () => {
     prompt.start();
 });
 
+//phone = result.phone + '@c.us';
+
 function sendMsg() {
-    prompt.get(['phone', 'text'], function(err, result) {
-        console.log(result);
-        let phone = result.phone + '@c.us';
-        let msg = result.text;
+    prompt.get(['phone'], function(err, result) {
+        let phone = result.phone;
+        
+        //manage commands
+        if (phone == '/help') {
+            console.log('help list');
+            repeatMsg();
+        } else if (phone == '/contacts') {
+            console.log(contacts);
+            repeatMsg();
 
-        client.sendMessage(phone, msg);
+        } else if (phone == '/addContact') {
+            prompt.get(['contactPhone', 'contactName'], function(err, result) {
+                contactPhone = result.contactPhone + '@c.us';
+                contactName = result.contactName;
 
-        repeatMsg();
+                newContact = {
+                    phone: contactPhone,
+                    name: contactName
+                }
+
+                contacts.push(newContact);
+
+                repeatMsg();
+            })
+
+        } else if (phone == '/msg') {
+            if (contacts.length === 0) {
+                console.log("You don't have any contacts yet. Use '/addContact' to create a new contact or just type a phone numbwe to chat with");
+                repeatMsg();
+            } else {
+                for (let i = 0; i < contacts.length; i++) {
+                    prompt.get(['contactName', 'message'], function(err, result) {
+                        if (result.contactName == contacts[i].name) {
+                            phone = contacts[i].phone;
+                            msg = result.message;
+
+                            client.sendMessage(phone, msg);
+
+                            repeatMsg();
+                        } else {
+                            console.log("You don't have any contact with that name");
+                            repeatMsg();
+                        }
+                    })
+                }
+            }
+
+        } else if (isNaN(phone) == false) {
+            prompt.get(['message'], function(err, result) {
+                phone = phone + '@c.us';
+                msg = result.message;
+                client.sendMessage(phone, msg);
+                repeatMsg();
+            });
+        } else {
+            console.log("That's not an existing command. Try '/help'");
+            repeatMsg();
+        }
     });
 }
 
@@ -34,7 +89,13 @@ function repeatMsg() {
 }
 
 client.on('message', message => {
-    console.log(message.from, message.body);
+    for (let i = 0; i < contacts.length; i++) {
+        if (message.from == contacts[i].phone) {
+            console.log(contacts[i].name + ': ', message.body);
+        } else {
+            console.log(message.from, message.body);
+        }
+    }
 });
 
 client.initialize();
